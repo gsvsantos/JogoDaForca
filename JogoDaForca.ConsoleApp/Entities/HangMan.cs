ced41a10fc1653d37c2f540e72d244a76602b290
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-17 21:32:14 -0300

    refactor: Refatoração completa do código. - Criada novas classes para melhor organização. - Criado menus interativos com o usuário.

---
+++ JogoDaForca.ConsoleApp/Entities/HangMan.cs
@@ -0,0 +1,178 @@
+﻿namespace JogoDaForca.ConsoleApp.Entities
+{
+    class HangMan
+    {
+        public static void MainMenu()
+        {
+            bool mainMenu = true;
+            do
+            {
+                Console.WriteLine("=--=--=--=--=--=--=--=--=--=--=--=");
+                Console.WriteLine("Menu Principal");
+                Console.WriteLine("=--=--=--=--=--=--=--=--=--=--=--=\n");
+                Console.WriteLine("1 >> Começar a Partida");
+                Console.WriteLine("2 >> Configurações");
+                Console.WriteLine("S >> Voltar à Tela Inicial");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.Clear();
+                        GameStart();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        GameConfiguration();
+                        break;
+                    case "S":
+                        Console.Clear();
+                        mainMenu = false;
+                        break;
+                }
+            } while (mainMenu);
+        }
+        public static void GameStart()
+        {
+            while (true)
+            {
+                Random random = new Random();
+
+                int chosenIndice = random.Next(Words.Fruits.Length);
+                string randomWord = Words.Fruits[chosenIndice];
+
+                char[] findLetters = new char[randomWord.Length];
+
+                for (int actualChar = 0; actualChar < findLetters.Length; actualChar++)
+                {
+                    findLetters[actualChar] = '_';
+                }
+
+                int errorsQuantity = 0;
+                bool playerWin = false;
+                bool playerLose = false;
+
+                do
+                {
+                    string head = errorsQuantity >= 1 ? " O " : " ";
+                    string body = errorsQuantity >= 2 ? "|" : " ";
+                    string leftArm = errorsQuantity >= 3 ? "/" : " ";
+                    string rightArm = errorsQuantity >= 4 ? @"\" : " ";
+                    string leftLeg = errorsQuantity >= 5 ? "/ " : " ";
+                    string rightLeg = errorsQuantity >= 6 ? "\\" : " ";
+
+                    Console.Clear();
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine("Jogo da Forca");
+                    Console.WriteLine(" ___________         ");
+                    Console.WriteLine(" |/        |         ");
+                    Console.WriteLine(" |        {0}        ", head);
+                    Console.WriteLine(" |        {0}{1}{2}  ", leftArm, body, rightArm);
+                    Console.WriteLine(" |         {0}       ", body);
+                    Console.WriteLine(" |        {0}{1}     ", leftLeg, rightLeg);
+                    Console.WriteLine(" |                   ");
+                    Console.WriteLine(" |                   ");
+                    Console.WriteLine("_|____               ");
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine($"Palavra escolhida: {String.Join("", findLetters)}");
+                    Console.WriteLine("-------------------------------------");
+                    if (errorsQuantity <= 5)
+                    {
+                        Console.WriteLine($"Erros do Jogador: {errorsQuantity}");
+                        Console.WriteLine("-------------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Você errou {errorsQuantity} vezes...");
+                        Console.WriteLine("-------------------------------------");
+                    }
+
+                    if (playerLose)
+                    {
+                        Console.WriteLine("Que pena, você enforcou o boneco =/");
+                        Console.WriteLine("-------------------------------------");
+                        break;
+                    }
+
+                    if (playerWin)
+                    {
+                        Console.WriteLine($"Você acertou a palavra {randomWord}, parabéns!");
+                        Console.WriteLine("-------------------------------------");
+                        break;
+                    }
+
+                    Console.Write("Digite uma letra: ");
+                    string letter = Console.ReadLine()!.ToUpper();
+                    if (letter.Length > 1)
+                    {
+                        Console.WriteLine("Precisa ser uma letra");
+                        Console.Write("Pressione [Enter] e tente novamente!");
+                        Console.ReadKey();
+                        continue;
+                    }
+
+                    char guess = letter[0];
+
+                    bool letterWasFound = false;
+
+                    for (int charCount = 0; charCount < randomWord.Length; charCount++)
+                    {
+
+                        if (guess == randomWord[charCount])
+                        {
+                            findLetters[charCount] = randomWord[charCount];
+                            letterWasFound = true;
+                        }
+                    }
+
+                    if (letterWasFound == false)
+                    {
+                        errorsQuantity++;
+                    }
+
+                    string wordFound = String.Join("", findLetters);
+
+                    playerWin = wordFound == randomWord;
+                    playerLose = errorsQuantity > 5;
+                } while (true);
+
+                Console.Write("Deseja jogar novamente? (S/N): ");
+                string optionContinue = Console.ReadLine().ToUpper();
+
+                while (optionContinue != "S" && optionContinue != "N")
+                {
+                    Console.Write("\nOpção inválida!\n\nPressione <Enter> e selecione novamente.");
+                    Console.Write("\nDeseja continuar? (S/N): ");
+                    optionContinue = Console.ReadLine().ToUpper();
+                }
+
+                if (optionContinue != "S")
+                    break;
+            }
+
+        }
+        public static void GameConfiguration()
+        {
+            Console.WriteLine("=--=--=--=--=--=--=--=--=--=--=--=");
+            Console.WriteLine("Configurações");
+            Console.WriteLine("=--=--=--=--=--=--=--=--=--=--=--=\n");
+            Console.WriteLine("1 >> Selecionar Tipo de Palavras (Padrão Frutas)");
+            Console.WriteLine("S >> Fechar o Jogo");
+            string option = Console.ReadLine()!;
+
+            if (option == "1")
+            {
+                Console.WriteLine("=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=");
+                Console.WriteLine("Selecione qual o tipo de palavras que queira jogar");
+                Console.WriteLine("=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=\n");
+                Console.WriteLine("Digite 1 para Frutas");
+                Console.WriteLine("Digite 2 para Animais");
+                Console.WriteLine("Digite 3 para Objetos");
+                Console.WriteLine("Digite 4 para Cores");
+                string typeWord = Console.ReadLine()!;
+                Words.WordRandomizer(typeWord);
+
+            }
+        }
+    }
+}
---
+++ JogoDaForca.ConsoleApp/Entities/Words.cs
@@ -0,0 +1,168 @@
+﻿namespace JogoDaForca.ConsoleApp.Entities
+{
+    public class Words
+    {
+        public static string[] Fruits =
+            [
+                "ABACATE",
+                "ABACAXI",
+                "ACEROLA",
+                "ACAI",
+                "ARACA",
+                "ABACATE",
+                "BACABA",
+                "BACURI",
+                "BANANA",
+                "CAJA",
+                "CAJU",
+                "CARAMBOLA",
+                "CUPUACU",
+                "GRAVIOLA",
+                "GOIABA",
+                "JABUTICABA",
+                "JENIPAPO",
+                "MACA",
+                "MANGABA",
+                "MANGA",
+                "MARACUJA",
+                "MURICI",
+                "PEQUI",
+                "PITANGA",
+                "PITAYA",
+                "SAPOTI",
+                "TANGERINA",
+                "UMBU",
+                "UVA",
+                "UVAIA"
+            ];
+
+        public static string[] Animals =
+            [
+                "CACHORRO",
+                "GATO",
+                "ELEFANTE",
+                "CAVALO",
+                "LEAO",
+                "TIGRE",
+                "ZEBRA",
+                "PANDA",
+                "LAGARTO",
+                "PASSARO",
+                "CANGURU",
+                "URSOS",
+                "JACARE",
+                "GIRAFA",
+                "HIPOPOTAMO",
+                "REINDEER",
+                "BUFALO",
+                "CROCODILO",
+                "LULA",
+                "TARTARUGA",
+                "PEIXE",
+                "PIRANHA",
+                "AQUARIO",
+                "GOLFINHO",
+                "BALLENA",
+                "PINGUIM",
+                "CORVO",
+                "EAGLE",
+                "GAVIAO",
+                "AGUIA"
+            ];
+
+        public static string[] Objects =
+            [
+                "CADEIRA",
+                "MESA",
+                "LAMPADE",
+                "SOFA",
+                "TV",
+                "CELULAR",
+                "TELEVISAO",
+                "COMPUTADOR",
+                "MICRO-ONDAS",
+                "FORNO",
+                "LIVRO",
+                "CANETA",
+                "LAPIS",
+                "FONE",
+                "RELOGIO",
+                "MAQUIAGEM",
+                "ESPONJA",
+                "TALHER",
+                "COLHER",
+                "FACAS",
+                "TIGELA",
+                "TACA",
+                "PRATO",
+                "CUPOM",
+                "ALMOFADA",
+                "PENDENTE",
+                "LIVRARIA",
+                "CARTAZ",
+                "MAPA"
+            ];
+
+        public static string[] Colors =
+            [
+                "VERMELHO",
+                "AZUL",
+                "AMARELO",
+                "VERDE",
+                "ROXO",
+                "LARANJA",
+                "BRANCO",
+                "PRETO",
+                "CINZA",
+                "MARRON",
+                "BEGE",
+                "DOURADO",
+                "PRATEADO",
+                "AZUL_CLARO",
+                "VERDE_CLARO",
+                "LILAS",
+                "PINK",
+                "MAGENTA",
+                "TURQUESA",
+                "INDIGO",
+                "CARAMELHO",
+                "LIMAO",
+                "PELE",
+                "FUCIA",
+                "CASTANHO",
+                "VIOLETA",
+                "MARROM_ESCURO",
+                "BRANCO_QUENTE",
+                "PRETO_ESCURO",
+                "AMARELO_CLARO"
+            ];
+        public static string WordRandomizer(string option)
+        {
+            Random random = new Random();
+            string randomWord = "";
+
+            if (option == "1")
+            {
+                int chosenIndice = random.Next(Fruits.Length);
+                randomWord = Fruits[chosenIndice];
+            }
+            if (option == "2")
+            {
+                int chosenIndice = random.Next(Fruits.Length);
+                randomWord = Animals[chosenIndice];
+            }
+            if (option == "3")
+            {
+                int chosenIndice = random.Next(Fruits.Length);
+                randomWord = Objects[chosenIndice];
+            }
+            if (option == "4")
+            {
+                int chosenIndice = random.Next(Fruits.Length);
+                randomWord = Colors[chosenIndice];
+            }
+
+            return randomWord;
+        }
+    }
+}
