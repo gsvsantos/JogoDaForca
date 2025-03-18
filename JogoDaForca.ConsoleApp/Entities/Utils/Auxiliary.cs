269da69719f9d3b230a7095b87f36405cb46020a
Gustavo Santos <gustavocnsantos02@gmail.com>
2025-03-17 23:44:21 -0300

    fix: Removido `using`s fantasmas.

--- JogoDaForca.ConsoleApp/Entities/Utils/Auxiliary.cs
+++ JogoDaForca.ConsoleApp/Entities/Utils/Auxiliary.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JogoDaForca.ConsoleApp.Entities.Utils
+﻿namespace JogoDaForca.ConsoleApp.Entities.Utils
 {
     class Auxiliary
     {
